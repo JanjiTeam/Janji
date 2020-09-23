164e4ff2a96af01923919e8f16f2a4b45d998601
Thomas Chapuis <chapuis.t@protonmail.com>
2020-09-23 17:21:53 +0200

    CS fix

--- src/Faker/Provider/ImmutableDateTime.php
+++ src/Faker/Provider/ImmutableDateTime.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace App\Faker\Provider;
 
 use Faker\Provider\Base;
@@ -11,6 +10,7 @@ final class ImmutableDateTime extends Base
     public static function immutableDateTimeBetween($startDate = '-30 years', $endDate = 'now', $timezone = null): \DateTimeImmutable
     {
         $newStartDate = $startDate instanceof \DateTimeImmutable ? \DateTime::createFromImmutable($startDate) : $startDate;
+
         return \DateTimeImmutable::createFromMutable(
             DateTime::dateTimeBetween($newStartDate, $endDate, $timezone)
         );
