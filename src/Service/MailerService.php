2188bf7fab8cbab3a997ada09263af6c40666e44
Thomas Chapuis <chapuis.t@gmail.com>
2021-01-06 16:23:31 +0100

    Added mailer service

---
+++ src/Service/MailerService.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
+class MailerService
+{
+    private MailerInterface $mailer;
+
+    public function __construct(MailerInterface $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
+    public function sendEventConfirmationMail()
+    {
+        $email = (new Email());
+
+        $this->mailer->send($email);
+    }
+}
